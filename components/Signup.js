e92d0029676f01099e0f9d449b7d23a0d48e56b0
abhay <abhaytxanax@gmail.com>
2023-02-11 14:55:22 +0530

    signup-component

---
+++ components/Signup.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <div className=' w-[500px] outline mx-auto my-24 bg-white text-gray-700 rounded-md'>
+            <h2 className='text-center text-4xl'>Sign in</h2>
+            <p className='text-center'>(user)</p>
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    Username
+                    </label>
+                    <input class="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                    </label>
+                    <input class="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                </div>
+                <div class="flex items-center justify-between">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Login
+                    </button>
+                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                        Login as Admin
+                    </a>
+                </div>
+            </form>
+        </div>
+      )
+}
+
+export default Signup
